0170af62eb9ebe04d3f18158cfafbb4055b62a69
nalinc <nalin.chhibber@gmail.com>
2018-05-28 02:53:26 -0400

    add config template

---
+++ public/config-template.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "<your-api-key>",
+  authDomain: "<auth-domain-url>",
+  databaseURL: "<database-url>",
+  projectId: "<project-id>",
+  storageBucket: "<storage-bucket>",
+  messagingSenderId: "<message-sender-id>"
+};
+firebase.initializeApp(config);
