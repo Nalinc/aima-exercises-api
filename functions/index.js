055791416f0d628ac493665268808e04f9a8a796
nalinc <nalin.chhibber@gmail.com>
2018-06-27 18:13:15 -0400

    add rating api

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,126 @@
 const functions = require('firebase-functions');
 const express = require('express');
+const admin = require('firebase-admin');
 const app = express();
+admin.initializeApp(functions.config().firebase);
+
+const db = admin.firestore();
+
+responseObj = {
+  "id": 0,
+  "label": "string",
+  "chapter": 0,
+  "last_modified": "2018-06-26T20:22:45.307Z",
+  "content": "string",
+  "type": "author_created",
+  "complete": false
+}
 
 app.get('/chapter/:chapterID',(request,response) => {
-	response.send("Hello world");
+	obj =  JSON.parse(JSON.stringify(responseObj))
+	obj["id"] = new Date().getTime()
+	obj["chapter"] = request.params.chapterID
+	response.send(obj);
 })
 
 app.delete('/chapter/:chapterID',(request,response) => {
-	response.send("Hello world");
+	obj =  JSON.parse(JSON.stringify(responseObj))
+	obj["id"] = new Date().getTime()
+	obj["chapter"] = request.params.chapterID
+	response.send(obj);
 })
 
 app.post('/chapter/:chapterID/exercise/:exerciseID',(request,response) => {
-	response.send("Hello world");
+	obj =  JSON.parse(JSON.stringify(responseObj))
+	obj["id"] = new Date().getTime()
+	obj["chapter"] = request.params.chapterID
+	obj["label"] = request.params.exerciseID
+	response.send(obj);
 })
 
-app.get('/hello',(request,response) => {
-	response.send("Hello world");
+app.get('/rating/:chapterID',(request,response) => {
+	obj = {
+	  "id": new Date().getTime(),
+	  "exercise": request.params.exerciseID,
+	  "chapter": request.params.chapterID,
+	  "rating": 0,
+	  "success": true
+	}
+	var docRef = db.collection('rating').doc(obj["chapter"]);
+	var getDoc = docRef.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        response.send(doc.data());
+      }
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+      response.send("ERROR");
+    });
+})
+
+app.get('/rating/:chapterID/:exerciseID',(request,response) => {
+	obj = {
+	  "id": new Date().getTime(),
+	  "exercise": request.params.exerciseID,
+	  "chapter": request.params.chapterID,
+	  "rating": 0,
+	  "success": true
+	}
+	var docRef = db.collection('rating').doc(obj["chapter"]);
+	var getDoc = docRef.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+        response.send('No such document!');
+      } else {
+        obj["rating"]=doc.data()[obj["exercise"]] || 0
+        response.send(obj);
+      }
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+      response.send("ERROR");
+    });
+})
+
+app.post('/rating/:chapterID/:exerciseID',(request,response) => {
+	obj = {
+	  "id": new Date().getTime(),
+	  "exercise": request.params.exerciseID,
+	  "chapter": request.params.chapterID,
+	  "rating": 0,
+	  "success": true
+	}
+	var docRef = db.collection('rating').doc(obj["chapter"]);
+	var getDoc = docRef.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+        response.send('No such document!');
+      } else {
+      	o = doc.data()
+      	if(o[obj["exercise"]]){
+      		o[obj["exercise"]]+=1
+      	}else{
+  			o[obj["exercise"]] = 1
+      	}
+      	docRef.set(o).then(()=>{
+      		obj["rating"] = o[obj["exercise"]]
+      		response.send(obj);
+      	})
+        // obj["rating"]=doc.data()[obj["exercise"]] || 0
+        // response.send(obj);
+      }
+    })
+    .catch(err => {
+      console.log('Error getting document', err);
+      response.send("ERROR");
+    });
+
+	response.send(obj);
 })
 
 // Create and Deploy Your First Cloud Functions
